573d24eef81b378f81c990f5cde7db231d99fc99
Abdul_Muheeth <abdul.muheeth@smartdocs.ai>
2023-05-21 02:39:07 +0530

    sweetalert added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,21 @@ import { ToastrService } from 'ngx-toastr';
 export class AppComponent {
 
   constructor( private toastr: ToastrService) {}
-  
+
   faCoffee =faCoffee;
 
   title = 'x-fe';
   
   showSuccess() {
     this.toastr.success('Hello world!', 'Toastr fun!');
+  }
+
+  showAlert() {
+    Swal.fire({
+      title: 'Error!',
+      text: 'Do you want to continue',
+      icon: 'error',
+      confirmButtonText: 'Cool'
+    })
   }
 }
